70214a401bf33dd4be16f2b36c7eccef59b6fb3a
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2026-09-20 23:57:30 -0300

    feat: add the client_id as a part of the key for don´t mix the items of the clients

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -1,18 +1,33 @@
 from common import message_protocol
-
+import uuid
 
 class MessageHandler:
 
     def __init__(self):
-        pass
+        self.client_id = uuid.uuid4().hex #32 aleatory characters
     
     def serialize_data_message(self, message):
+        """
+        Serialize the message adding a client id
+        """
         [fruit, amount] = message
-        return message_protocol.internal.serialize([fruit, amount])
+        return message_protocol.internal.serialize([(self.client_id, fruit), amount])
 
     def serialize_eof_message(self, message):
-        return message_protocol.internal.serialize([])
+        """
+        Serialize the EOF message adding a client id to know which client sent the EOF message.
+        """
+        return message_protocol.internal.serialize([self.client_id,None])
 
     def deserialize_result_message(self, message):
+        """
+        Deserialize the message and check if it is for this client.
+        In case it is not for this client, returns None, otherwise returns the fruit and amount.
+        """
         fields = message_protocol.internal.deserialize(message)
-        return fields
+        [(id_client,fruit),amount] = fields
+
+        if id_client != self.client_id:
+            return None
+        
+        return [fruit, amount]
